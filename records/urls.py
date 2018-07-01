b97160fc347617bcd382308e8e1364e9244b7fcc
siyingq <e0235267@u.nus.edu>
2018-07-01 20:11:42 +0800

    commit attempt 1

--- records/urls.py
+++ records/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name='index'), #1
 
 ]
+
+'''
+using #1 allows us to link our home page from any other page by
+creating the following link in the template:
+<a href="{% url 'index' %}">Home</a>
+
+'''
--- records/views.py
+++ records/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Category, SittingDate, ParliamentRecord
 
 # Create your views here.
+def index(request): #home page of website
+    #wishful thinking: drop down select bar for all the categories available
+    num_records = ParliamentRecord.objects.all().count()
+    num_categories = Category.objects.all().count()
+    all_categories = ParliamentRecord.objects.all()
+
+    return render(
+        request, 
+        index.html, 
+        context={'num_records':num_records, 'num_categories':num_categories},
+        )
